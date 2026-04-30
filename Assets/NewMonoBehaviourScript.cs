26b395ed853b901813b1e73054d3ef1b136a487c
dorisngleen <s10271165@connect.np.edu.sg>
2026-04-30 14:41:56 +0800

    task 3

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -5,6 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     Vector3 newPosition = new Vector3(0.005f, 0.005f, 0.005f);
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,12 +18,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         transform.position += newPosition;
+        transform.Rotate(0, 100, 0);
 
         if(transform.position.x > 0.5)
         {
             newPosition.x = -0.005f;
             newPosition.y = -0.005f;
             newPosition.z = -0.005f;
+
+            transform.Rotate(0, 100, 0);
         }
 
     }
