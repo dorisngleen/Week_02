b4261a0ed005d3d4876de9ca07d7ded6d4616023
dorisngleen <s10271165@connect.np.edu.sg>
2026-05-28 15:06:30 +0800

    week 5 task 1

--- Assets/Scripts/CollisionScript.cs
+++ Assets/Scripts/CollisionScript.cs
@@ -2,47 +2,64 @@ using UnityEngine;
 
 public class CollisionScript : MonoBehaviour
 {
-
+    AudioSource collectibleAudio;
     public static int cylindersLeft = 4;
+
+    void Start()
+    {
+        cylindersLeft = 4;
+        collectibleAudio = GetComponent<AudioSource>();
+    }
+
+    public void Collect()
+    {
+        if (collectibleAudio != null && collectibleAudio.clip != null)
+        {
+            AudioSource.PlayClipAtPoint(collectibleAudio.clip, transform.position);
+        }
+        Destroy(gameObject);
+    }
 
     void OnCollisionEnter(Collision collision)
     {
-      print("Collision detected with" + collision.gameObject.name);
+        print("Collision detected with " + collision.gameObject.name);
 
-      if(collision.gameObject.name == "NestedParent_Unpack")
-      {
-        Destroy(gameObject);
-      }
-
-      if(collision.gameObject.name.Contains("Cylinder"))
-      {
-        cylindersLeft--;
-        print("Cylinder collected! Remaining: " + cylindersLeft);
-        //Destroy(collision.gameObject);
-      } 
-
-      if (collision.gameObject.name == "FinishZone")
+        if (collision.gameObject.name == "NestedParent_Unpack")
         {
-            if (cylindersLeft <= 0)
-            {
-                print("You collected everything! Well done!");
-            }
-            else
-            {
-                print("Still " + cylindersLeft + " cylinders left!");
-            }
+            Destroy(gameObject);
+            return;
         }
 
+        if (collision.gameObject.name.Contains("Cylinder"))
+        {
+            cylindersLeft--;
+            print("Cylinder collected! Remaining: " + cylindersLeft);
+
+            CollisionScript cylinderScript = collision.gameObject.GetComponent<CollisionScript>();
+            if (cylinderScript != null)
+                cylinderScript.Collect();
+            else
+                Destroy(collision.gameObject);
+
+            return;
+        }
+
+        if (collision.gameObject.name == "FinishZone")
+        {
+            if (cylindersLeft <= 0)
+                print("You collected everything! Well done!");
+            else
+                print("Still " + cylindersLeft + " cylinders left!");
+        }
     }
 
     void OnCollisionExit(Collision collision)
     {
-      print("Colliding ended with" + collision.gameObject.name);
-      Destroy(gameObject);
+        print("Colliding ended with " + collision.gameObject.name);
     }
 
     void OnCollisionStay(Collision collision)
     {
-      print("Colliding with" + collision.gameObject.name);
+        print("Colliding with " + collision.gameObject.name);
     }
 }
